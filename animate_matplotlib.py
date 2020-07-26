e3674e4bd5ca9a8ea4f8932e4056f6157a02dd2b
devansh-dvj <devansh.dvj@gmail.com>
2020-07-27 01:41:08 +0530

    blit=False for changing axes as well.

--- animate_matplotlib.py
+++ animate_matplotlib.py
@@ -10,16 +10,19 @@ dir = os.path.dirname(__file__)
 exoplanets = __import__('exoplanets').system
 
 
-def update_graph(num, total, scat):
+def update_graph(num, total, scat, ax):
     # size_array = [1 for i in range(total)]
     # for i in range(total):
     #     if num % total == i:
     #         size_array[i] = 10
     size_array = [10 if num % total == i else 1 for i in range(total)]
-    return scat,
     color_array = ['r' if num % total > i else 'b' for i in range(total)]
     scat[0].set_sizes(size_array)
     scat[1].set_color(color_array)
+
+    ax[0].set(title='Axes 1 : {}'.format(num))
+    ax[1].set(title='Axes 2 : {}'.format(num))
+    return scat, ax
 
 
 params_file = '1.yaml'
@@ -31,11 +34,17 @@ time_coord = system.coords()
 timespan = system.timespan
 lum = system.output()
 
-scat = plt.scatter(timespan, lum, s=1, c='b')
+axes, scatters = [], []
 fig = plt.figure(figsize=(10, 10))
 
+axes.append(fig.add_axes([0.6, 0.1, 0.3, 0.3]))  # ax[0]
+axes.append(fig.add_axes([0.1, 0.6, 0.3, 0.3]))  # ax[1]
+
+scatters.append(axes[0].scatter(timespan, lum, s=1, c='b'))  # scat[0]
+scatters.append(axes[1].scatter(timespan, lum, s=1, c='b'))  # scat[1]
+
 anim = animation.FuncAnimation(fig, update_graph, frames=len(timespan), interval=2,
-                               repeat=True, fargs=(len(timespan), scat), blit=True)
+                               repeat=True, fargs=(len(timespan), scatters, axes), blit=False)
 
 ffmpeg_writer = animation.FFMpegWriter(fps=50)
 now = time.strftime('%Y_%m_%d_%H_%M', time.localtime(time.time()))
