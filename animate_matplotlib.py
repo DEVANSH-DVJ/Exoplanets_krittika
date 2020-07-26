9e7312447de7c22ec6387e2f89fed4958ba031f5
devansh-dvj <devansh.dvj@gmail.com>
2020-07-26 20:36:33 +0530

    Started Animation using Matplotlib

---
+++ animate_matplotlib.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import time
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+dir = os.path.dirname(__file__)
+
+exoplanets = __import__('exoplanets').system
+
+
+def update_graph(num, total, scat):
+    # size_array = [1 for i in range(total)]
+    # for i in range(total):
+    #     if num % total == i:
+    #         size_array[i] = 10
+    size_array = [10 if num % total == i else 1 for i in range(total)]
+    scat.set_sizes(size_array)
+    return scat,
+
+
+params_file = '1.yaml'
+if len(sys.argv) > 1:
+    params_file = sys.argv[1]
+
+system = exoplanets(params_file, time_split=100, img_split=100, n=1.0)
+time_coord = system.coords()
+timespan = system.timespan
+lum = system.output()
+
+fig = plt.figure()
+scat = plt.scatter(timespan, lum, s=1, c='b')
+
+anim = animation.FuncAnimation(fig, update_graph, frames=len(timespan), interval=2,
+                               repeat=True, fargs=(len(timespan), scat), blit=True)
+
+ffmpeg_writer = animation.FFMpegWriter(fps=50)
+now = time.strftime('%Y_%m_%d_%H_%M', time.localtime(time.time()))
+anim.save('{}.mp4'.format(now), writer=ffmpeg_writer)
